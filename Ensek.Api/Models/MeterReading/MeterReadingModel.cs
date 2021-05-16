eacb2816c64e901bc3d8cc086e5e82989d2ad39d
Martin <martin.erskine@gmail.com>
2021-05-16 16:56:44 +0100

    Add final TODO

--- Ensek.Api/Models/MeterReading/MeterReadingModel.cs
+++ Ensek.Api/Models/MeterReading/MeterReadingModel.cs
@@ -3,14 +3,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Ensek.Api.Models.MeterReading
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class MeterReadingModel
     {
+        /// <summary>
+        /// 
+        /// </summary>
         [Required]
         public int AccountId { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         [Required]
         public DateTime MeterReadingDateTime { get; set; }
 
+
+        //TODO: Determine Business requirement to deal with Read values greater then 99999.
+
+        /// <summary>
+        /// 
+        /// </summary>
         [Required, Range(1, 99999)]
         public int MeterReadValue { get; set; }
     }
